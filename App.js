9a76bda849f86d59dcc4de00d8b230c4aff28a58
Daniil Ovoshchnikov <daniil@lightbase.nl>
2018-10-18 20:04:43 +0200

    dispaly recognized text

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Dimensions, TouchableOpacity} from 'react-native';
+// @flow
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import Svg, { Rect, G, Text as SVGText } from 'react-native-svg';
 
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 type Props = {};
 
@@ -18,51 +12,77 @@ type State = {
   isFrontFacing: boolean,
   canDetectText: boolean,
   canDetectFaces: boolean,
-}
+};
 
 export default class App extends Component<Props, State> {
   state = {
     isFrontFacing: true,
     canDetectText: false,
     canDetectFaces: false,
-  }
+    textBlocks: [],
+  };
 
   facesDetected = face => {
     console.log(face);
   };
 
-  textRecognized = text => {
-    console.log(text);
+  textRecognized = object => {
+    const { textBlocks } = object;
+    this.setState({ textBlocks });
   };
 
-  toggle = value => this.setState((prevState) => ({[value]: !prevState[value]}));
+  toggle = value => () => this.setState(prevState => ({ [value]: !prevState[value] }));
+
+  setRef = ref => () => (this.camera = ref);
+
+  renderBlock = textBlock => (
+    <G key={textBlock.value + textBlock.bounds.origin.x}>
+      <Rect
+        x={textBlock.bounds.origin.x}
+        y={textBlock.bounds.origin.y}
+        width={textBlock.bounds.size.width}
+        height={textBlock.bounds.size.height}
+        fill="rgba(0,0,0,0)"
+        strokeWidth="1"
+        stroke="rgb(255,0,0)"
+      />
+      <SVGText fill="red" x={textBlock.bounds.origin.x} y={textBlock.bounds.origin.y}>
+        {textBlock.value}
+      </SVGText>
+    </G>
+  );
 
   render() {
-    const { isFrontFacing, canDetectFaces, canDetectText } = this.state;
+    const { isFrontFacing, canDetectFaces, canDetectText, textBlocks = [] } = this.state;
     return (
       <View style={styles.container}>
         <RNCamera
-            ref={ref => {
-              this.camera = ref;
-            }}
-            style={{ flex: 1, width, justifyContent: 'flex-end', }}
-            type={isFrontFacing ? RNCamera.Constants.Type.front : RNCamera.Constants.Type.back}
-            // faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
-            onFacesDetected={canDetectFaces ? this.facesDetected : null}
-            onTextRecognized={canDetectText ? this.textRecognized : null}
+          ref={this.setRef}
+          style={{ flex: 1, width, justifyContent: 'flex-end' }}
+          type={isFrontFacing ? RNCamera.Constants.Type.front : RNCamera.Constants.Type.back}
+          // faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
+          onFacesDetected={canDetectFaces ? this.facesDetected : null}
+          onTextRecognized={canDetectText ? this.textRecognized : null}
         >
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={() => this.toggle('isFrontFacing')} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}> Flip Camera </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.toggle('canDetectFaces')} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}>{!canDetectFaces ? 'Detect Faces' : 'Detecting Faces'}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.toggle('canDetectText')} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}>{!canDetectText ? 'Detect Text' : 'Detecting Text'}</Text>
-          </TouchableOpacity>
-        </View>
+          <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+            <TouchableOpacity onPress={this.toggle('isFrontFacing')} style={styles.capture}>
+              <Text style={{ fontSize: 14 }}> Flip Camera </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.toggle('canDetectFaces')} style={styles.capture}>
+              <Text style={{ fontSize: 14 }}>
+                {!canDetectFaces ? 'Detect Faces' : 'Detecting Faces'}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.toggle('canDetectText')} style={styles.capture}>
+              <Text style={{ fontSize: 14 }}>
+                {!canDetectText ? 'Detect Text' : 'Detecting Text'}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </RNCamera>
+        <Svg style={{ position: 'absolute', height: height - 40, width, top: 0, left: 0 }}>
+          {textBlocks.map(block => this.renderBlock(block))}
+        </Svg>
       </View>
     );
   }
@@ -75,23 +95,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
   capture: {
     flex: 0,
     backgroundColor: '#fff',
     borderRadius: 5,
     padding: 15,
-    // paddingHorizontal: 20,
     alignSelf: 'center',
-    // margin: 20,
   },
 });
